5639a29910656a95944cfb1808ab60a831a9062f
Alebraco <122585245+Alebraco@users.noreply.github.com>
2025-03-23 18:21:46 -0400

    .

--- phagepickr/data_collection/receptors_function.py
+++ phagepickr/data_collection/receptors_function.py
@@ -1,25 +1,41 @@
 from phagepickr.data_collection.protein_ids import retrieve_ids
 from phagepickr.data_collection.protein_names import retrieve_titles, fix_unnamed
 
-def receptors(query, recs = 50):
-    '''Retrieve receptor titles and sequences for a specific pathogenic host. Use
+def receptors(query, recs=50):
+    """Retrieve receptor titles for a specific pathogenic host using
     previously defined functions.
-    
+
     Args:
-      query (str): A string used to query the database. The format should match 
-        the specific requirements of the database.
-      maxrec (int, optional): The number of records to retrieve for each batch.
-      
+        query (str): A string used to query the database. The format should match 
+            the specific requirements of the database.
+        recs (int, optional): The number of records to retrieve for each batch.
+
     Returns:
-      tuple: A tuple containing two lists. The first list contains the protein 
-        titles. The second list contains the corresponding amino acid sequences.
-    
-    '''
-    ids = retrieve_ids(query, db = 'ipg', maxrec = recs)
-    titles = retrieve_titles(ids, db = 'ipg', maxrec = recs)
-    titles = fix_unnamed(titles)
-    
-    return titles
+        list: A list containing the protein titles.
+    """
+    try:
+        print(f"Querying database with query: {query}")
+        ids = retrieve_ids(query, db='ipg', maxrec=recs)
+        print(f"Retrieved IDs")
+        if not ids:
+            print("No IDs retrieved.")
+            return []
+
+        print("Retrieving titles for the retrieved IDs.")
+        titles = retrieve_titles(ids, db='ipg', maxrec=recs)
+        print(f"Retrieved titles")
+        if not titles:
+            print("No titles retrieved.")
+            return []
+
+        print("Fixing unnamed titles.")
+        titles = fix_unnamed(titles)
+        print(f"Final titles obtained.")
+        return titles
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
 
 
 
