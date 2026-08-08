93f629a3b8b94ca62b8ea83fa4605f085cf1ed9c
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-08-08 16:55:10 -0400

    Update output.py

--- phagepickr/utils/output.py
+++ phagepickr/utils/output.py
@@ -1,15 +1,25 @@
-def print_phage_cocktail(product, phageinfo, target):
+def print_phage_cocktail(product, phageinfo, target, calls = None, accessions = None):
     flag = False
+    selected = set(accessions) if accessions else set()
+    accns = {record['phage']:record['acc'] for record in phageinfo if record['acc'] in selected}
+
     for phage in product:
         is_known = False
         for record in phageinfo:
             if record['phage'] == phage and record['host']== target:
                 is_known = True
                 break
+        name = f'{phage}*' if is_known else phage
         if is_known:
-            print(f"{phage}*")
             flag = True
+
+        if calls and accns.get(phage) in calls:
+            virulent, temperate, call = calls[accns[phage]]
+            print(f'{name} - {call} (virulent: {virulent:.3f}, temperate: {temperate:.3f})')
         else:
-            print(phage)
+            print(name)
+
     if flag:
         print('Confirmed phages are marked with an asterisk (*)')
+    if calls:
+        print('Warning: Lifestyle predictions might be inaccurate.')
